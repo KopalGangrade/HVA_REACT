d603e82f2a51dd98bbff93173a841aec44a16688
kopal <kopalgangrade0@gmail.com>
2023-11-30 10:51:37 +0530

    class_component

---
+++ classcomponent/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+
+// CLASS COMPONENT 
+
+class App extends React.Component
+{
+  render(){
+    return <h1>Hello , class Component</h1>
+  }
+}
+
+export default App;
